6df1f0a9e978fc112c1f84f769b09b9cfdae1e98
Praveen8104 <praveenreddygoli8@gmail.com>
2024-07-22 15:50:57 +0530

    Resume

--- Template_2/script.js
+++ Template_2/script.js
@@ -2,11 +2,6 @@ document.getElementById('download').addEventListener('click', () => {
     const element = document.querySelector('.resume_body');
     const options = {
         filename: 'Resume.pdf',
-        jsPDF: {
-            unit: 'px',
-            format: 'a4',
-            orientation: 'portrait'
-        }
     };
     html2pdf().from(element).set(options).save();
 });
