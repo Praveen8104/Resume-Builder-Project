4ca63639ef2dd011d6d5afe60c8af0aada36838b
Praveen8104 <praveenreddygoli8@gmail.com>
2024-07-25 10:13:32 +0530

    Aditya Resume

--- Main_Template_5/script.js
+++ Main_Template_5/script.js
@@ -1,7 +1,4 @@
 document.getElementById('download').addEventListener('click', () => {
     const element = document.querySelector('.resume');
-    const options = {
-        filename: 'MainResume_5.pdf',
-    };
-    html2pdf().from(element).set(options).save();
+    html2pdf().from(element).save('MainResume_5.pdf');
 });
